acc21d40be5a21dd678b3314a00b90ecd32957bc
baalaal <147219396+baalaal@users.noreply.github.com>
2025-03-24 00:35:42 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Muhammad Iqbal Al Khaththath]");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
